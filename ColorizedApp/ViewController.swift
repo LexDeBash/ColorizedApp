b3dfd91835b7502ec75e057fe2a3f35e1c3a1a1a
Alexey Efimov <lex.efimov@gmail.com>
2019-07-02 15:43:05 +0500

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Alexey Efimov on 02/07/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
